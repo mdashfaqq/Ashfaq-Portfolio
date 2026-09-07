8287029dfa429e5f0d6985045fa3bbe575b7e8a3
Mohamed Ashfaq <ash47306@gmail.com>
2026-09-08 00:18:27 +0530

    feat: initialize App component with router and Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { AppRouter } from "@/routes/AppRouter";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import { MobileSplashScreen } from "@/components/ui/MobileSplashScreen";
 
 export default function App() {
   return (
     <>
-      <MobileSplashScreen />
+
       <AppRouter />
       <SpeedInsights />
     </>
